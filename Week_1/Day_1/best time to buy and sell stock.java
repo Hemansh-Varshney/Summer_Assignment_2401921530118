931e65b9af4f8d10d0fdbdade8f76379049a57ec
Hemansh Varshney <hemanshvarsh32@gmail.com>
2026-06-02 22:12:00 +0530

    Add solution for best time to buy and sell stock

---
+++ Week_1/Day_1/best time to buy and sell stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int min=prices[0];
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(prices[i]<min)
+            min=prices[i];
+
+            max=Math.max(max,prices[i]-min);
+        }
+        return max;
+        
+    }
+}
