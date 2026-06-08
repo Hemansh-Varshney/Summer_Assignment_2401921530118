1c60003c10d52e4849aa4b165d8a9839f7a70185
Hemansh Varshney <hemanshvarsh32@gmail.com>
2026-06-08 16:10:31 +0530

    Add kiduser class for managing kids library accounts

---
+++ Week_1/OOPS_Problem_1/kiduser.java
@@ -0,0 +1,25 @@
+class kiduser implements libraryuser {
+
+    int age;
+    String booktype;
+
+    kiduser(int age, String booktype) {
+        this.age = age;
+        this.booktype = booktype;
+    }
+
+    public void registerAccount() {
+        if (age < 12) {
+            System.out.println("You have successfully registered under a Kids Account");
+        } else {
+            System.out.println("Sorry, Age must be less than 12 to register as a kid");
+        }
+    }
+    public void requestBook() {
+        if (booktype.equalsIgnoreCase("Kids")) {
+            System.out.println("Book Issued successfully, please return the book within 10 days");
+        } else {
+            System.out.println("Oops, you are allowed to take only kids books");
+        }
+    }
+}
