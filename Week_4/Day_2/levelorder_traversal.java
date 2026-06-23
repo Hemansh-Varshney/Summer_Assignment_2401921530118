c3fb58b07c13445ade342e0d7cc8fbaea5664e58
Hemansh Varshney <hemanshvarsh32@gmail.com>
2026-06-23 08:13:16 +0530

    Add level order traversal method in Solution class
    Implement level order traversal for a binary tree.

---
+++ Week_4/Day_2/levelorder_traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null)
+        return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            while(size>0)
+            {
+                TreeNode node =queue.poll();
+                if(node.left!=null)
+                queue.add(node.left);
+
+                if(node.right!=null)
+                queue.add(node.right);
+
+                list.add(node.val);
+                size--;
+            }
+            res.add(list);
+        }
+        return res;
+        
+    }
+}
